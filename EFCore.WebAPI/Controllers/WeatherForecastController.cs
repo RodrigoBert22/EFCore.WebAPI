118bd8cbf7a3642e3f6cce04e7317144d1017ac1
Rodrigo Bertolini <rodrigobertolini12@hotmail.com>
2019-11-25 22:05:07 -0300

    HttpGET / Context

--- EFCore.WebAPI/Controllers/WeatherForecastController.cs
+++ EFCore.WebAPI/Controllers/WeatherForecastController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCore.Dominio;
+using EFCore.Repo;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,17 +13,22 @@ namespace EFCore.WebAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        public readonly HeroiContext _context;
+        public WeatherForecastController(HeroiContext context)
+        {
+            _context = context;
+        }
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        //private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
@@ -35,5 +42,17 @@ namespace EFCore.WebAPI.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("{nomeHeroi}")]
+        public ActionResult Get(string nomeHeroi)
+        {
+            var heroi = new Heroi { Nome = nomeHeroi };
+            
+                _context.Herois.Add(heroi);
+                _context.SaveChanges();
+            
+            return Ok();
+        }
+
     }
 }
